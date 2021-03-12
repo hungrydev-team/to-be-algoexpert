2c574ab12c9c9efbe05dd0bc34a577fa37fb030b
SHIN BEOMSOO <sdalbsoo@gmail.com>
2021-03-13 01:50:12 +0900

    queue update

--- sbs/data_structure/queue.py
+++ sbs/data_structure/queue.py
@@ -1,6 +1,16 @@
 from abc import abstractmethod
+from typing import Optional
 
-from data_structure.node import Node
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class NoSuchElementException:
+    pass
 
 
 class QueueInterface:
@@ -26,40 +36,61 @@ class MyQueue:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.size = 0
 
     def is_empty(self):
         return self.head is None
 
-    def enqueue(self, item):
-        # item을 리스트의 끝부분에 추가한다.
-        new_node = Node(item)
+    def enqueue(self, node) -> None:
+        self.size += 1
         if self.head is None:
-            self.head = new_node
-            self.tail = self.head
+            self.head = node
+            self.tail = node
+            return
         else:
-            self.tail.next = new_node
-            self.tail = self.tail.next
+            self.tail.prev = node
+            node.next = self.tail
+            self.tail = node
+            return
 
-    def dequeue(self):
-        # 리스트의 첫 번재 항목을 제거한다.
+    def dequeue(self) -> Optional[Node]:
         if self.is_empty():
             return
-        temp = self.head
-        self.head = temp.next
-        if self.head is None:
+        if self.size == 1:
+            node = self.tail
             self.tail = None
+            self.head = None
+            self.size -= 1
+            return node
+        else:
+            node = self.head
+            self.head = node.prev
+            node.prev.next = None
+            self.size -= 1
+            return node
+
+    def search(self, index):
+        if index < 0 or self.size < index:
+            raise NoSuchElementException()
+        else:
+            cursor = self.tail
+            for i in range(index):
+                cursor = cursor.next
+            return cursor
 
     def print_queue(self):
-        temp = self.head
+        temp = self.tail
+        print("tail -> ", end="")
         while temp:
-            print(temp.data, "-> ", end='')
+            print(temp.data, "-> ", end="")
             temp = temp.next
+        print("head")
 
 
 queue = MyQueue()
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-queue.enqueue(4)
-queue.dequeue()
+queue.enqueue(Node(1))
+queue.enqueue(Node(2))
+queue.enqueue(Node(3))
+queue.enqueue(Node(4))
+queue.enqueue(Node(4))
 queue.print_queue()
