542418ede796f07a4f1e7289938256895924ed24
SHIN BEOMSOO <sdalbsoo@gmail.com>
2021-03-26 17:56:26 +0900

    search 메서드 구현

--- sbs/data_structure/binary_tree.py
+++ sbs/data_structure/binary_tree.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Node:
     def __init__(self, data: int) -> None:
         self.data = data
@@ -31,13 +34,30 @@ class BinaryTree(object):
     def delete(self):
         pass
 
-    def search(self):
-        pass
+    def search(self, node: Node) -> Optional[bool]:
+        cursor = self.root
+        while cursor.data is not None:
+            if cursor.data == node.data:
+                return True
+            else:
+                if cursor.data < node.data:
+                    if cursor.right is not None:
+                        cursor = cursor.right
+                    else:
+                        return False
+                else:
+                    if cursor.left is not None:
+                        cursor = cursor.left
+                    else:
+                        return False
 
 
 array = [51, 2, 7, 10, -31, 0, 8]
 binary_tree = BinaryTree()
 for i in array:
     binary_tree.insert(Node(i))
-
+a = binary_tree.search(Node(51))
+print(a)
+b = binary_tree.search(Node(1))
+print(b)
 
