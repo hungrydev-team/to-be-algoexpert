974b7e8eed6e6d3b50ad7a7a156b982f0e60a163
SHIN BEOMSOO <sdalbsoo@gmail.com>
2021-04-04 03:57:15 +0900

    delete node WIP

--- sbs/data_structure/binary_tree.py
+++ sbs/data_structure/binary_tree.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 
 class Node:
@@ -11,6 +11,7 @@ class Node:
 class BinaryTree(object):
     def __init__(self):
         self.root = None
+        self.curr_node = None
 
     def insert(self, node: Node) -> None:
         cursor = self.root
@@ -31,8 +32,24 @@ class BinaryTree(object):
                     cursor.left = node
                     break
 
-    def delete(self):
-        pass
+    def delete(self, key) -> Node:
+        self.root, is_deleted = self._delete_value(self.root, key)
+        return is_deleted
+
+    @staticmethod
+    def _delete_value(node: Node, key: int) -> Tuple[Optional[Node], bool]:
+        if node is None:
+            return node, False
+        deleted = False
+
+        if key == node.data:
+            deleted = True
+            pass
+        elif key < node.data:
+            pass
+        else:
+            pass
+        return node, deleted
 
     def search(self, node: Node) -> Optional[bool]:
         cursor = self.root
@@ -56,8 +73,9 @@ array = [51, 2, 7, 10, -31, 0, 8]
 binary_tree = BinaryTree()
 for i in array:
     binary_tree.insert(Node(i))
-a = binary_tree.search(Node(51))
-print(a)
-b = binary_tree.search(Node(1))
-print(b)
-
+# a = binary_tree.search(Node(51))
+# print(a)
+# b = binary_tree.search(Node(1))
+# print(b)
+c = binary_tree.delete(Node(51))
+print(c.data)
